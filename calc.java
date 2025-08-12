96439c0c59a8dcd9f20d56f6a1ef0fa9c08c5216
Darsh Saxena <161448152+DarshSaxena@users.noreply.github.com>
2025-08-12 15:47:46 +0530

    Add files via upload

---
+++ calc.java
@@ -0,0 +1,25 @@
+class calc
+{
+public static void main(String args[])
+{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[2]);
+switch(args[0])
+{
+case "+":
+System.out.println(+(a+b));
+ break;
+case "-":
+System.out.println(+(a-b));
+break;
+case "*":
+System.out.println(+(a*b));
+break;
+case "/":
+System.out.println(+(a/b));
+break;
+default:
+System.out.println("incorrect entry");
+}
+}
+}

69063a3a051162c48754de49781dc959c1c7f319
Darsh Saxena <161448152+DarshSaxena@users.noreply.github.com>
2025-08-17 17:46:30 +0530

    Add files via upload

---
+++ area.java
@@ -0,0 +1,10 @@
+class Area
+{
+ public static void main(String args[])
+{
+int h=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+double area=0.5*b*h;
+System.out.print("area of triangle is"+area);
+}
+}
